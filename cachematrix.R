ba9b562ac7f3f8290b1ab8cbedf17e4d31555793
DrFractal <KnightOfChess@gmail.com>
2015-03-14 17:15:26 -0400

    Both functions work
    Functions work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,133 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache 
+##   its inverse.
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+## Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
 
+## We assume that the matrix supplied is always invertible.
+
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache 
+##   its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  getx <- function() x
+  setx <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getinverse <- function() inverse
+  setinverse <- function(inv){
+    inverse <<- inv
+  }
+  list(getx = getx, 
+       setx = setx, 
+       getinverse = getinverse, 
+       setinverse = setinverse) ## return the list of sub-functions
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: This function computes the inverse of the special "matrix" returned 
+##   by makeCacheMatrix above. 
+##   If the inverse has already been calculated (and the matrix has not changed), 
+##   then the cachesolve should retrieve the inverse from the cache.
+## If the matrix in x is not set, it will return NULL
+## Return a matrix that is the inverse of 'x', assumes x is invertible
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if( !is.null(inverse) ){
+    message("returning inverse from cache")
+    return(inverse)
+  }
+  data <- x$getx()
+  if(!is.null(data)){        ## check to be sure x data isn't null
+    inverse <- solve(data)   ## find the inverse at last!
+    x$setinverse(inverse)    ## set the inverse, so it's cached
+  }
+  inverse                    ## return the inverse
 }
+
+## ## Test 1 ##
+# m1 <- matrix( 
+# c(2, 4, 3, 1), # the data elements 
+# nrow=2,        # number of rows 
+# ncol=2,        # number of columns 
+# byrow = TRUE)  # fill matrix by rows
+# matt <- makeCacheMatrix(m1)
+# cacheSolve(matt)
+# cacheSolve(matt) ## do it again and show off the cache
+## ## Test 1 Result ##
+##      [,1] [,2]
+## [1,] -0.1  0.4
+## [2,]  0.3 -0.2
+
+## ## Test 2 ## 2x2 Identity Matrix
+# m1 <- matrix( 
+# c(1, 0, 0, 1), # the data elements 
+# nrow=2,        # number of rows 
+# ncol=2,        # number of columns 
+# byrow = TRUE)  # fill matrix by rows
+# matt <- makeCacheMatrix(m1)
+# cacheSolve(matt)
+# cacheSolve(matt) ## do it again and show off the cache
+## ## Test 2 Result ##
+#      [,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
+
+## ## Test 3 ## 3x3 Identity Matrix
+# m1 <- matrix( 
+# c(1, 0, 0, 
+#   0, 1, 0,
+#   0, 0, 1),    # the data elements 
+# nrow=3,        # number of rows 
+# ncol=3,        # number of columns 
+# byrow = TRUE)  # fill matrix by rows
+# matt <- makeCacheMatrix(m1)
+# matt$getx()
+# cacheSolve(matt)
+# cacheSolve(matt) ## do it again and show off the cache
+## ## Test 3 Result ##
+#      [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    0    1    0
+# [3,]    0    0    1
+# 
+# ## do it again and show off the cache
+# returning inverse from cache
+#      [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    0    1    0
+# [3,]    0    0    1
+
+## ## Test 4 ## 3x3 Matrix: Rotate about z by angle, shrinking or expanding by r
+# r <- (sqrt(5) -1 )/2
+# angle <- (pi / 4)
+# y1 <- (r * sin(angle))
+# x1 <- (r * cos(angle))
+# m1 <- matrix(
+# 
+# c(x1, -y1, 0, 
+#   y1, x1, 0,
+#   0, 0, 1),    # the data elements 
+# nrow=3,        # number of rows 
+# ncol=3,        # number of columns 
+# byrow = TRUE)  # fill matrix by rows
+# matt <- makeCacheMatrix(m1)
+# matt$getx()
+# cacheSolve(matt)
+# cacheSolve(matt) ## do it again and show off the cache
+# ## ## Test 4 Result ##
+#           [,1]     [,2] [,3]
+# [1,]  1.144123 1.144123    0
+# [2,] -1.144123 1.144123    0
+# [3,]  0.000000 0.000000    1
+# ## do it again and show off the cache
+# returning inverse from cache
+#           [,1]     [,2] [,3]
+# [1,]  1.144123 1.144123    0
+# [2,] -1.144123 1.144123    0
+# [3,]  0.000000 0.000000    1
